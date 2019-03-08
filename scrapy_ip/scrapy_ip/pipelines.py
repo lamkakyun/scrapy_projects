bf6deeb0a89c18ee3298d4c744cdd7a35271d9f3
lamkakyun <lamkakyun@gmail.com>
2019-03-08 18:18:56 +0800

    n

--- scrapy_ip/scrapy_ip/pipelines.py
+++ scrapy_ip/scrapy_ip/pipelines.py
@@ -1,11 +1,27 @@
 # -*- coding: utf-8 -*-
 
-# Define your item pipelines here
-#
-# Don't forget to add your pipeline to the ITEM_PIPELINES setting
-# See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
+import sqlite3
 
 
 class ScrapyIpPipeline(object):
+
+    def __init__(self):
+        self.name = 'ip_pipeline'
+
+    # @classmethod
+    # def from_crawler(cls, crawler):
+    #     """ create a pipeline instance"""
+    #     # return cls(
+    #     #     sqlite_conn = sqlite3.connect(crawler.settings.get('SQLITE_PATH'))
+    #     # )
+    #     pass
+
+    def open_spider(self, spider):
+        print("%s: %s" % (self.name, 'open spider'))
+
+    def close_spider(self, spider):
+        print("%s: %s" % (self.name, 'close spider'))
+
     def process_item(self, item, spider):
+        print(item)
         return item
--- scrapy_ip/scrapy_ip/spiders/IpSpider1.py
+++ scrapy_ip/scrapy_ip/spiders/IpSpider1.py
@@ -1,20 +1,34 @@
 # -*- coding: utf-8 -*-
-import scrapy
-from scrapy.spiders import Rule
+from scrapy.spiders import Rule, CrawlSpider
 from scrapy.linkextractors import LinkExtractor
+from scrapy.loader import ItemLoader
+# from ..misc import config as conf
+from scrapy_ip.items import ScrapyIpItem
 
-class Ipspider1Spider(scrapy.Spider):
+class Ipspider1Spider(CrawlSpider):
     name = 'IpSpider1'
     allowed_domains = ['www.xicidaili.com']
     start_urls = [
         'http://www.xicidaili.com/nn', # 高匿名
-        'https://www.xicidaili.com/nt/', # 普通
-        ]
+        # 'https://www.xicidaili.com/nt/', # 普通
+    ]
 
     rules = [
         # Rule(LinkExtractor(allow=('http://www.xicidaili.com/nn/\d+$'), callable='parse')),
         # Rule(LinkExtractor(allow=('http://www.xicidaili.com/nt/\d+$')), callable='parse')
     ]
 
-    def parse(self, response):
-        pass
+    def parse_start_url(self, response):
+        data = response.xpath('//table[@id="ip_list"]/tr')
+        
+        for d in data:
+            item_loader = ItemLoader(item=ScrapyIpItem(), selector=d)
+            item_loader.add_xpath('ip', 'td[2]/text()')
+            item_loader.add_xpath('port', 'td[3]/text()')
+            item_loader.add_xpath('hide_type', 'td[5]/text()')
+            item_loader.add_xpath('is_http', 'td[6]/text()')
+            item = item_loader.load_item()
+
+            # print(item)
+
+            yield item
